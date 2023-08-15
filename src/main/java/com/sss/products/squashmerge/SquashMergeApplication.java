e82988f76e1a792ea55d7a40108de6f4a157237d
Rodel Castro <m88.shang.rodeca@gmail.com>
2023-08-15 15:34:46 +0800

    DG-0001 Initial project commit

---
+++ src/main/java/com/sss/products/squashmerge/SquashMergeApplication.java
@@ -0,0 +1,13 @@
+package com.sss.products.squashmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquashMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquashMergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sss/products/squashmerge/SquashMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sss.products.squashmerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquashMergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
