78ff3a412398229d49ed4a37788c59014a7eb514
Rodel Castro <m88.shang.rodeca@gmail.com>
2023-08-15 18:07:05 +0800

    DG-0004 Complete person attributes

--- src/main/java/com/sss/products/squashmerge/domain/entities/Person.java
+++ src/main/java/com/sss/products/squashmerge/domain/entities/Person.java
@@ -17,5 +17,11 @@ public class Person {
 
 	@Column(name = "first_name")
 	private String firstName;
+	
+	@Column(name = "middle_name")
+	private String middleName;
+	
+	@Column(name = "last_name")
+	private String lastName;
 
 }
